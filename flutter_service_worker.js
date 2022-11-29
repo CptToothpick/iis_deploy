1bdc7ee64d04604c4d6ec88b96d08a8689937e9e
Martin Laštovica <vonakom@gmail.com>
2022-11-29 03:53:40 +0100

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,9 +15,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3d470c9dfa66aa3c3ca3e978dfc313ab",
-"/": "3d470c9dfa66aa3c3ca3e978dfc313ab",
-"main.dart.js": "b3eaf890d7d29b7809c7af61908d9720",
+"index.html": "4de0f3071b48fad97c718f1ef8a7ea4b",
+"/": "4de0f3071b48fad97c718f1ef8a7ea4b",
+"main.dart.js": "f87d7b30ba3c5d40b189f50ac34974a4",
 "manifest.json": "d32eec7c53399cf5ceda3bc88cd461e1",
 "version.json": "faa38fb3bfa9abda1d9ac3a450ae46c3"
 };
